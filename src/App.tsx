c8a277d8a817b9ad839ceebe7c1380656ecc16ff
Stiliyan Kushev <stiliyankushev@gmail.com>
2020-06-27 02:53:19 +0300

    fixed a wrongly named file

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
 import { AppState, ReduxProps } from './reducers';
 
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import CustomRoute, { CustomRouteProps } from './shared/PrivateRoute';
+import CustomRoute, { CustomRouteProps } from './shared/CustomRoute';
 
 // IMPORT SOME GENERAL COMPONENTS
 import LoadingView from './shared/LoadingView';
