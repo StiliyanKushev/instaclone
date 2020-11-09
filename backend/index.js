0f5dbb03c693795b39504d888c61513a76eb654f
StiliyanKushev <stiliyankushev@gmail.com>
2020-11-09 13:04:40 +0000

    Removed some console logs

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,6 @@ const io = socketio(http);
 
 io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
-      console.log(name,room);
       const { error, user } = addUser({ id: socket.id, name, room });
   
       if(error) return callback(error);
@@ -40,14 +39,7 @@ io.on('connect', (socket) => {
     socket.on('sendMessage', (message, callback) => {
       const user = getUser(socket.id);
 
-<<<<<<< HEAD
       if(!user){
-=======
-      console.log(user);
-
-      if(!user){
-        console.log(socket.id);
->>>>>>> master
         return callback("User not found");
       }
   
